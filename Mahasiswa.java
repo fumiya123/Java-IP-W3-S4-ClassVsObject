f917ee8b055bb56dfb4595706ef63f233e563a3d
fumiya123 <98727343+fumiya123@users.noreply.github.com>
2024-09-11 23:32:18 +0700

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+    String nama;
+    int nim;
+    String jurusan;
+
+    // Method untuk menampilkan informasi mahasiswa
+    void infoMahasiswa() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Jurusan: " + jurusan);
+    }
+}
